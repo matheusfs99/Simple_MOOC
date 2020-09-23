5ac65bf0c02c2d690c871b18146c459f5d483c07
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-23 18:56:43 -0300

    adicionado links das urls no template com namespace

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contato/', views.contact, name='contact'),
+    path('', include('core.urls', namespace='core')),
     path('admin/', admin.site.urls),
 ]
