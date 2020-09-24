0b8d310ca72ed826e15a65922fbec3805ccda9e2
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-24 18:30:32 -0300

    customização no Manager

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class CourseManager(models.Manager):
+    def search(self, query):
+        return self.get_queryset().filter(models.Q(name__icontains=query) |
+                                          models.Q(description__icontains=query))
+
+
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Nome')
     slug = models.SlugField(verbose_name='Atalho')
     description = models.TextField(verbose_name='Descrição', blank=True)
     start_date = models.DateField(verbose_name='Data de Início', null=True, blank=True)
-    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem')
+    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
     created_at = models.DateTimeField(verbose_name='Criado em', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Atualizado em', auto_now=True)
+
+    objects = CourseManager()
