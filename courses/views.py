9d3e091a38284c20d25ada3d1400f58319ea8548
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-25 18:32:05 -0300

    criada página de cursos

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+def index(request):
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    template_name = 'courses/index.html'
+    return render(request, template_name, context)
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls', namespace='core')),
+    path('cursos/', include('courses.urls', namespace='courses')),
     path('admin/', admin.site.urls),
 ]
