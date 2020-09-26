243571486e604fcb8e69be4556bcf371cfcc59fe
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-25 21:09:35 -0300

    criado form de fale conosco do curso

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 # Create your views here.
 def index(request):
@@ -12,8 +13,13 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    if request.POST:
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
     context = {
-        'course': course
+        'course': course,
+        'form': form
     }
     template_name = 'courses/details.html'
     return render(request, template_name, context)
