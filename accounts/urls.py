3ff9bd9834afd950e3cac4177b0a62f7fee7271f
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-28 17:27:45 -0300

    criado view e template de cadastro de user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import *
 
 app_name = 'accounts'
 urlpatterns = [
     path('entrar/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('cadastre-se/', register, name='register'),
 ]
