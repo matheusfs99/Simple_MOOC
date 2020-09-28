a7b3eb25a457d299062231195f92cb09a02b7e8c
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-28 18:23:03 -0300

    inserido campo de email no form de cadastro com validação de unique

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
     template_name = 'register.html'
     context = {}
-    form = UserCreationForm()
+    form = RegisterForm()
     if request.POST:
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(settings.LOGIN_URL)
